0912368e6f3190f827c11bd0fb2fc9d3b11f554d
Elvis Valenzuela <Elvis.valenzuela23@gmail.com>
2025-02-19 19:51:13 -0500

    Fuctins in JavaScript

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+function calculateTax(amount) {
+    const taxRate = 0.10; // 10% tax
+    return amount * taxRate;
+}
 
+function convertToUpperCase(text) {
+    return text.convertToUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return Math,max(num1, num2);
+}
 
+function isPalindrome(word) {
+// Remove non-alphanumeric characters and convert to lowercase
+const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+// Compare the cleaned word with its reverse
+return
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = (originalPrice * discountPercentage) / 100;
+    return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
